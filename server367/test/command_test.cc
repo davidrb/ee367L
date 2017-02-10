32df1628d26ff92fd70e4aeadb4cfb27d5c05de0
David Badke <davidrbadke@gmail.com>
2017-02-09 18:44:05 -1000

    refactor tests

--- server367/test/command_test.cc
+++ server367/test/command_test.cc
@@ -1,8 +1,15 @@
 #include "catch.hpp"
 #include "../src/command.hpp"
 
-TEST_CASE("Runs a command and return output as string", "") {
+TEST_CASE("Returns output as string", "") {
   auto cmd = Command{"/usr/bin/ls", "ls", "WORKSPACE"};
   REQUIRE( cmd.get_output() == "WORKSPACE\n" );
+}
+
+TEST_CASE("Returns successful status") {
+  auto cmd = Command{"/usr/bin/ls", "ls"};
   REQUIRE( cmd.get_return_status() == 0 );
+
+  cmd = Command{"/usr/bin/ls", "ls", "non-existant"};
+  REQUIRE( cmd.get_return_status() != 0 );
 }
